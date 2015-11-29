140bed9a68b0a12e12d95513ef2fe9049f45b87a
André Freire <and__re@hotmail.com>
2015-11-29 06:25:12 -0200

    que sono

--- src/Writer.java
+++ src/Writer.java
@@ -10,9 +10,9 @@ public class Writer extends Thread{
 		int key = random.nextInt(1000000000);
 		
 		BD.writers.put(key,BD.writers.size());
-		System.out.println(String.valueOf(BD.inUse)+"-"+BD.readers.size()+"-"+(BD.writers.get(key)));
-		while(!BD.isAuthorization() || BD.readers.size() != 0 || BD.writers.get(key) > 0);
-			System.out.println(String.valueOf(BD.inUse)+"-"+BD.readers.size()+"-"+(BD.writers.get(key)));		
+		while(!BD.isAuthorization() || BD.readers.size() != 0 || BD.writers.get(key) > 0){
+			System.out.println(BD.inUse+"-"+BD.readers.size()+"-"+BD.writers.get(key));
+		};		
 		System.out.println("Entrando na regiao critica wt");
 		for(int i = 0; i < 100; i++){
 			BD.setItem(random.nextInt(BD.getSizeDatabase()));
@@ -23,7 +23,7 @@ public class Writer extends Thread{
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		BD.writers.remove(key);
+		//BD.writers.remove(key);
 		System.out.println("Saindo da regiao critica - wt");
 		BD.decrementWriters();
 
